4f1ab77a0ca80a3f0d45a9dcf3ed12b28774bdb4
MehadiHasan-Micro <mhimrannazir@gmail.com>
2021-01-22 17:37:03 +0600

    fst

---
+++ assignment.js
@@ -0,0 +1,19 @@
+
+
+// project 1: kilometerToMeter
+
+function kilometerToMeter(km){
+    var meter = km * 1000;
+    return meter;
+}
+console.log(kilometerToMeter(2));
+
+
+
+
+// project 2: budgetCalculator
+
+// project 3: hotelCost
+
+// project 4: megaFriend
+
